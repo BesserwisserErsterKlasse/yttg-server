590c8bbeb865773c92817fe0be82e14dbc679f48
Emelianov Artem <yaz008.yaz008@yandex.ru>
2026-04-11 01:34:46 +0300

    Add main

---
+++ src/main.py
@@ -0,0 +1,59 @@
+# flake8: noqa: E402
+# isort: skip_file
+import asyncio
+
+asyncio.set_event_loop(asyncio.new_event_loop())  # Python3.14 behavior fix
+
+from asyncio import run
+from pathlib import Path
+
+from project.globalobjects import server, tg
+from project.pipelines import download_video, get_streams, select_stream
+from project.server.types import (
+    DownloadRequest,
+    DownloadResponse,
+    Stream,
+    StreamInfoRequest,
+    StreamInfoResponse,
+    UnmatchedRequestError,
+    YttgRequest,
+)
+
+
+@server.on_request(StreamInfoRequest)
+async def stream_info_request_handler(request: StreamInfoRequest) -> None:
+    async with tg.acquire(request.provider) as chat:
+        streams: dict[Stream, tuple[int, int]] | None = await get_streams(chat, request)
+        if streams is None:
+            return None
+        await server.send(
+            peer_id=request.peer_id,
+            response=StreamInfoResponse(streams=list(streams.keys())),
+        )
+
+
+@server.on_request(DownloadRequest)
+async def download_handler(request: DownloadRequest) -> None:
+    async with tg.acquire(request.provider) as chat:
+        streams: dict[Stream, tuple[int, int]] | None = await get_streams(chat, request)
+        if streams is None:
+            return None
+        await select_stream(chat, streams[request.stream])
+        savepath: Path | None = await download_video(chat, request)
+        if savepath is None:
+            return None
+        await server.send(request.peer_id, DownloadResponse(savepath=savepath))
+
+
+@server.on_unmatched_request
+async def unmatched_request_handler(request: YttgRequest) -> None:
+    await server.send(peer_id=request.peer_id, response=UnmatchedRequestError(request))
+
+
+async def main() -> None:
+    await tg.start()
+    await server.start()
+
+
+if __name__ == '__main__':
+    run(main())
